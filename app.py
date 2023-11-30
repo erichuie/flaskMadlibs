2a229a1be9533f1c1a8b7998341e5f3378fa88fd
Max Shpungin <maxsdance@gmail.com>
2023-11-29 16:54:34 -0800

    after code review

--- app.py
+++ app.py
@@ -11,19 +11,20 @@ debug = DebugToolbarExtension(app)
 @app.get('/')
 def madlib_form():
     """Renders form for madlib story prompts"""
-    print("test")
+    print("test")#no spaces is convention for passing vars to templates
     return render_template("questions.html", prompts = silly_story.prompts)
 
 @app.get('/results')
 def display_results():
     """Renders resulting story from user prompts"""
     #need form answers
-    answers_dict = {}
+    #answers_dict = {}
+ #FIXME: basically this was all extra because we can just pass the args
+ # to get_result_text
+    # for prompt in silly_story.prompts:
+    #     answer = request.args.get(prompt)
+    #     answers_dict[prompt] = answer
 
-    for prompt in silly_story.prompts:
-        answer = request.args.get(prompt)
-        answers_dict[prompt] = answer
-
-    results_text = silly_story.get_result_text(answers_dict)
+    results_text = silly_story.get_result_text(request.args)
 
     return render_template('results.html', results_text=results_text)
