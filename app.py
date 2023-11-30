d0ddc1f9315463dc339b8fb15d1b0e250682e636
Max Shpungin <maxsdance@gmail.com>
2023-11-29 16:19:40 -0800

    completed results rout

--- app.py
+++ app.py
@@ -10,5 +10,20 @@ debug = DebugToolbarExtension(app)
 
 @app.get('/')
 def madlib_form():
+    """Renders form for madlib story prompts"""
     print("test")
     return render_template("questions.html", prompts = silly_story.prompts)
+
+@app.get('/results')
+def display_results():
+    """Renders resulting story from user prompts"""
+    #need form answers
+    answers_dict = {}
+
+    for prompt in silly_story.prompts:
+        answer = request.args.get(prompt)
+        answers_dict[prompt] = answer
+
+    results_text = silly_story.get_result_text(answers_dict)
+
+    return render_template('results.html', results_text=results_text)
